aacc27a1e50dca9bf6efb3bfe128a9e7076ed8f8
David Garcia <david.garcia@abank.com.sv>
2021-09-22 17:52:39 -0600

    Reductores en redux

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,8 +1,16 @@
+import { ADD_ARTICLE } from '../constants/action-types';
+
 const initialState = {
   articles: []
 };
 
 const rootReducer = (state = initialState, action) => {
+  if (action.type === ADD_ARTICLE) {
+    return {
+      articles: [...action.payload]
+    };
+  }
+
   return state;
 };
 
