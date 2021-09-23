d4ea858840f26ece060fa8f3fc3ab51daf844a5b
David Garcia <davidjosue.garcia@outlook.com>
2021-09-22 19:17:30 -0600

    Componente de lista y estado de Redux

--- src/components/List.js
+++ src/components/List.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const List = () => {
+const mapStateToProps = (state) => {
+  return {
+    articles: state.articles
+  };
+};
+
+const ConnectList = ({ articles }) => {
   return (
-    <div>
-      
-    </div>
+    <ul>
+      {articles.map(el => (
+        <li key={el.id}>{el.title}</li>
+      ))}
+    </ul>
   );
 };
+
+const List = connect(mapStateToProps)(ConnectList);
 
 export default List;
