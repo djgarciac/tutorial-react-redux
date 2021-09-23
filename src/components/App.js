041b7e031653cb5dd239b4ebcec82cf78fa41202
David Garcia <davidjosue.garcia@outlook.com>
2021-09-22 19:07:31 -0600

    Componente de la aplicación y tienda redux

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import List from './List';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h2>Articles</h2>
+
+      <List />
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-import index from './js/index';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import store from './js/store/index';
+import App from './components/App';
+import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
